ad8fd916d32676b1e5d8f0f2d7d6df30fe6dce84
jakub.koziara <jakub.koziara@sklepopon.com>
2018-11-21 19:55:03 +0100

    Add run from process with file write

--- etl/spiders/works.py
+++ etl/spiders/works.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.crawler import CrawlerProcess
 
 
 class JobItem(scrapy.Item):
@@ -21,9 +22,20 @@ class WorksSpider(scrapy.Spider):
             item["location"] = oneJobOffer.css("h3.teaser__location::text").extract()
             item["price"] = oneJobOffer.css("p.teaser__price::text").extract()
             item["params"] = oneJobOffer.css(".teaser__params > li::text").extract()
+
             yield item
 
         # next_page_url = response.css(".pagination__nextPage::attr(href)").extract_first()
         # if next_page_url:
         #     yield scrapy.Request(url=next_page_url, callback=self.parse)
 
+
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+    'FEED_FORMAT': 'json',
+    'FEED_URI': 'data.json'
+})
+
+process.crawl(WorksSpider)
+process.start()  # the script will block here until the crawling is finished
+print('smierdzace dupsko Kamila')
