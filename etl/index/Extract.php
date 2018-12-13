8cc6907ebeb0792dd4778620c4468092ce9956ff
jakub.koziara <jakub.koziara@sklepopon.com>
2018-12-13 18:27:59 +0100

    Exctract to class

--- etl/index/Extract.php
+++ etl/index/Extract.php
@@ -1,13 +1,21 @@
 <?php
-class Extract {
-	public function __construct(){}
-	
-	public function extract() {
-      $pyscript = 'C:\Users\jakub.koziera\Documents\Projects\Scrapy\crawler\etl\spiders\works.py';
-      $python = 'C:\Users\jakub.koziera\AppData\Local\Continuum\anaconda3\python.exe';
-      $cmd = "$python $pyscript";
-      exec($cmd, $output);
-	}
+
+class Extract
+{
+    public function __construct()
+    {
+    }
+
+    public function extract()
+    {
+        //clear file
+        file_put_contents('data.json', '');
+
+        $pyscript = 'C:\Users\jakub.koziera\Documents\Projects\Scrapy\crawler\etl\spiders\works.py';
+        $python = 'C:\Users\jakub.koziera\AppData\Local\Continuum\anaconda3\python.exe';
+        $cmd = "$python $pyscript";
+        exec($cmd, $output);
+    }
 }
 
 
